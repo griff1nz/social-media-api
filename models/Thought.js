ec258cf173ff70c99dfb0e4f1cbedc5f15f8c0be
Zion Griffin <defendingmymaininbox@gmail.com>
2024-03-04 18:31:00 -0500

    Added thought schema

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => date.toLocaleDateString('en-us')
+    },
+    username: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 25,
+    },
+    reactions: [{
+        type: Schema.Types.ObjectId,
+        ref: 'reaction',
+    }],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+}
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought;
